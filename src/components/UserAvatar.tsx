1efaea541da30e21dd361bc8ccc2e4e1b8995f4a
David <jdm.juridico@gmail.com>
2026-02-07 02:49:35 -0600

    test: cambio obvio para diagnóstico de deployment

--- src/components/UserAvatar.tsx
+++ src/components/UserAvatar.tsx
@@ -125,7 +125,7 @@ export function UserAvatar() {
                             className="flex items-center gap-3 px-4 py-2 text-charcoal-700 hover:bg-gray-50 transition-colors"
                         >
                             <User className="w-4 h-4" />
-                            <span>Mi perfil</span>
+                            <span>MI PERFIL TEST</span>
                         </Link>
                         <Link
                             href="/precios"
